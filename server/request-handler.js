8ac40406680228020c7581a347ec8fe06f67421d
macqueen <jessmacqueen@gmail.com>
2013-09-03 11:01:39 -0700

    basic server functionality

---
+++ server/request-handler.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var fs = require('fs');
+
+var exp = module.exports.handleRequest = express();
+var filePath = '/Users/Jess/Documents/hack_reactor/todo'
+var todos = [];
+
+exp.use(express.bodyParser());
+
+exp.get('/', function(req, res){
+  fs.readFile(filePath + '/public/index.html', function(error, content){
+  	if(!error){
+  		res.set('Content-Type', 'text/html');
+  		res.send(200, content);
+  	} else {
+  		res.send(500, error);
+  	}
+  });
+});
+
+exp.get('/app/todos.js', function(req, res){
+  fs.readFile(filePath + '/public/app/todos.js', function(error, content){
+  	if(!error){
+  		res.set('Content-Type', 'application/javascript');
+  		res.send(200, content);
+  	} else {
+  		res.send(500, error);
+  	}
+  });
+});
+
+exp.post('/', function(req, res){
+	todos.push(req.body.todo);
+	res.redirect('/');
+  res.send(200);
+})
---
+++ server/server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+var handlers = require("./request-handler")
+
+var port = 8080;
+var ip = "127.0.0.1";
+var server = http.createServer(handlers.handleRequest);
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
