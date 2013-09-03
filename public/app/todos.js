ae93bc2f65f66d7f0afb59f2f4851f81d55ac4b6
macqueen <jessmacqueen@gmail.com>
2013-09-03 11:02:35 -0700

    added name to input form

--- public/app/todos.js
+++ public/app/todos.js
@@ -1,5 +1,5 @@
 
 $(document).on('ready', function(){
 	$('<div></div>').text('Add a new todo item!').appendTo('.container');
-	$('<div></div>').append('<form method = "POST"><input type="input"></input></form>').appendTo('.container');
+	$('<div></div>').append('<form method ="POST"><input type="input" name="todo"></input></form>').appendTo('.container');
 });
