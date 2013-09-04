eb30211f8243f4211613890d1e9b7ff3d4eed915
macqueen <jessmacqueen@gmail.com>
2013-09-03 18:52:42 -0700

    updated client side to load todos from db

--- public/app/todos.js
+++ public/app/todos.js
@@ -1,5 +1,28 @@
 
 $(document).on('ready', function(){
+  // render the form
 	$('<div></div>').text('Add a new todo item!').appendTo('.container');
 	$('<div></div>').append('<form method ="POST"><input type="input" name="todo"></input></form>').appendTo('.container');
+  
+  //request existing todos
+  var getTodos = function(){
+  	$.ajax('http://127.0.0.1:8080/all/todos', {
+      success: function(data){
+        renderTodos(data);
+      },
+      error: function(){
+        console.log('error!');
+      }
+  	})
+  };
+  
+  var renderTodos = function(data){
+    $('.listContainer').html('');
+    for (var i = 0; i < data.itemList.length; i++){
+    	$('<li></li>').text(data.itemList[i].todoItem)
+    	              .appendTo('.listContainer');
+    }
+  };
+
+  getTodos();
 });
